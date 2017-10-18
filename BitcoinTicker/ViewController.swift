b3b0a364b0942ce0a31eca7544ee63e84422942b
Renan Avrahami <renan.avrahami@gmail.com>
2017-10-17 22:04:20 -0400

    Create outlets for price label and UIPicker

--- BitcoinTicker/ViewController.swift
+++ BitcoinTicker/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var bitcoinPriceLabel: UILabel!
+    @IBOutlet weak var currencyPicker: UIPickerView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
